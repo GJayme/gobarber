6736fbd04de3c39af812cb47ecb63e2a9878f0b1
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-02-04 22:05:12 -0300

    Loader de model usuário.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gabriel Jayme',
+    email: 'gabrielj_89@hotmail.com',
+    password_hash: '123465484',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
