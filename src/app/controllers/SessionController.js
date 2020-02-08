fc82c0ea1645e76eb05ead932b51b3784216bf99
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-02-08 12:04:09 -0300

    Correção autenticação JWT.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -4,7 +4,7 @@ import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
-  async sotre(req, res) {
+  async store(req, res) {
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
