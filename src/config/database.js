537283118d8dad8ec5c5c6635a9d29a68758d5af
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-02-10 11:36:01 -0300

    Ajustes na configuração do Docker.

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  // IP do Docker:
+  host: '172.17.0.2', 
+  // Porta do Docker:
+  port: '5432',
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
